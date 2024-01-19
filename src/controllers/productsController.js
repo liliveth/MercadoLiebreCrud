84ff9c1ed52308c486e785102521b226eb8f533c
liliveth <lili.49_@outlook.com>
2024-01-19 03:38:26 -0300

    MercadoLiebre validaciones y multer

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 const fs = require("fs");
 const path = require("path");
 
@@ -21,35 +23,46 @@ const controller = {
   },
 
   // Create - Form to create
+
   create: (req, res) => {
     res.render("product-create-form");
-    // Do the magic
   },
 
   // Create -  Method to store
-  store: (req, res) => {
-    const { name, price, discount, description, category } = req.body;
-    // let products = loadProducts();
-    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-    const newProduct = {
-      id: products[products.length - 1].id + 1,
-      name: name.trim(),
-      price: +price,
-      description: description.trim(),
-      discount: +discount,
-      category,
-      image: "default-image.png",
-    };
-    productsModify = [...products, newProduct];
-    // storeProducts(productsModify);
-    const productjson = JSON.stringify(productsModify);
-    fs.writeFileSync(
-      path.join(__dirname, "../data/productsDataBase.json"),
-      productjson,
-      "utf-8"
-    );
-    return res.redirect("/products");
-    // Do the magic
+  store: (req, res, next) => {
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+      res.render("product-create-form", {
+        errores: errores.mapped(),
+        old: req.body,
+      });
+    } else {
+      const file = req.file;
+      const { name, price, discount, description, category } = req.body;
+      // if (!file){
+      //   throw new Error("Por favor seleccione un archivo")
+      // }
+      // let products = loadProducts();
+      const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+      const newProduct = {
+        id: products[products.length - 1].id + 1,
+        name: name.trim(),
+        price: +price,
+        description: description.trim(),
+        discount: +discount,
+        category,
+        image: file ? file.filename : "default-image.png",
+      };
+      productsModify = [...products, newProduct];
+      // storeProducts(productsModify);
+      const productjson = JSON.stringify(productsModify);
+      fs.writeFileSync(
+        path.join(__dirname, "../data/productsDataBase.json"),
+        productjson,
+        "utf-8"
+      );
+      return res.redirect("/products");
+    }
   },
 
   // Update - Form to edit
@@ -57,7 +70,6 @@ const controller = {
     let { id } = req.params;
     let product = products.find((element) => element.id == id);
     res.render("product-edit-form", { product });
-    // Do the magic
   },
   // Update - Method to update
   update: (req, res) => {
@@ -84,23 +96,23 @@ const controller = {
       "utf-8"
     );
     return res.redirect("/products/detail/" + req.params.id);
-
-    // Do the magic
   },
 
   // Delete - Delete one product from DB
   destroy: (req, res) => {
     let { id } = req.params;
+    const product = products.find((producto) => producto.id == id);
     let productsModify = products.filter((product) => product.id != id);
     const productjson = JSON.stringify(productsModify);
+    fs.unlink("./public/images/products/" + product.image, (err) => {
+      if (err) throw err;
+    });
     fs.writeFileSync(
       path.join(__dirname, "../data/productsDataBase.json"),
       productjson,
       "utf-8"
     );
     return res.redirect("/products");
-
-    // Do the magic
   },
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,65 @@
 // ************ Require's ************
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const { check, body, validationResult } = require("express-validator");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../", "../public/images/products"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+  },
+});
+const upload = multer({ storage });
 
 // ************ Controller Require ************
 const productsController = require("../controllers/productsController");
+
+const validationCreate = [
+  body("name")
+    .notEmpty()
+    .withMessage("este campo es obligatorio")
+    .bail()
+    .isLength({ min: 3, max: 30 })
+    .withMessage("debe tener min 3 carácteres y máx 30")
+    .bail(),
+  body("price")
+    .notEmpty()
+    .withMessage("este campo es obligatorio")
+    .bail()
+    .isNumeric()
+    .withMessage("debe ingresar un valor númerico")
+    .bail(),
+  body("discount")
+    .notEmpty()
+    .withMessage("este campo es obligatorio")
+    .bail()
+    .isNumeric()
+    .withMessage("debe ingresar un valor númerico")
+    .bail(),
+  body("description")
+    .notEmpty()
+    .withMessage("este campo es obligatorio")
+    .bail()
+    .isLength({ min: 5, max: 100 })
+    .withMessage("debe tener min 5 carácteres y máx 100")
+    .bail(),
+];
 
 /*** GET ALL PRODUCTS ***/
 router.get("/", productsController.index);
 
 /*** CREATE ONE PRODUCT ***/
 router.get("/create", productsController.create);
-router.post("/create", productsController.store);
+router.post(
+  "/create",
+  upload.single("image"),
+  validationCreate,
+  productsController.store
+);
 
 /*** GET ONE PRODUCT ***/
 router.get("/detail/:id", productsController.detail);
